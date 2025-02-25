13bfcef8e31595122498ebbfe49a54ce22cca412
Msnewbee <bilariko2@gmail.com>
2025-02-26 06:48:17 +0700

    Create Navbar.tsx

---
+++ public/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <a href="/loby">Loby</a>
+            <a href="/vidio">Vidio</a>
+            <a href="/kontak">Kontak</a>
+        </nav>
+    );
+}
