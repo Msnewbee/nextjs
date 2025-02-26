e0087fb5dd20d34965c0cc6a8a8e55de45352fa3
Msnewbee <bilariko2@gmail.com>
2025-02-26 12:14:34 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { useState } from 'react';
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
-import Video from '../components/Video';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Video from './components/Video';
 
 export default function Home() {
     const [activeTab, setActiveTab] = useState('loby');
