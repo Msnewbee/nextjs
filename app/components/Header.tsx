e565f7bf6c112171741ad6d3f0441100c880f662
Msnewbee <bilariko2@gmail.com>
2025-02-26 12:52:32 +0700

    Create Header.tsx

---
+++ app/components/Header.tsx
@@ -0,0 +1,8 @@
+// components/Header.tsx
+export default function Header({ title = "Selamat Datang di iClik Bos!" }) {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    );
+}
