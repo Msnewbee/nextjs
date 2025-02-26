171896be995c1391ec12e67080e07e5a298f194c
Msnewbee <bilariko2@gmail.com>
2025-02-26 11:53:37 +0700

    Update Video.tsx

--- public/Video.tsx
+++ public/Video.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+
 export default function Video({ title, thumbnail, videoId }: { title: string; thumbnail: string; videoId: string }) {
+    const [likeCount, setLikeCount] = useState(0);
+    const [dislikeCount, setDislikeCount] = useState(0);
+
     return (
         <div className="content-box">
             <h3>{title}</h3>
@@ -6,8 +11,12 @@ export default function Video({ title, thumbnail, videoId }: { title: string; th
                 <img src={thumbnail} alt={`Thumbnail ${title}`} />
             </a>
             <div>
-                <button className="like-btn">👍 <span>0</span></button>
-                <button className="dislike-btn">👎 <span>0</span></button>
+                <button className="like-btn" onClick={() => setLikeCount(likeCount + 1)}>
+                    👍 <span>{likeCount}</span>
+                </button>
+                <button className="dislike-btn" onClick={() => setDislikeCount(dislikeCount + 1)}>
+                    👎 <span>{dislikeCount}</span>
+                </button>
             </div>
         </div>
     );
