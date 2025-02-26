f462ef95651b5be450f154c7a9aea78c701f3851
Msnewbee <bilariko2@gmail.com>
2025-02-26 12:54:00 +0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,1 +1,12 @@
+// components/Navbar.tsx
+import Link from 'next/link';
 
+export default function Navbar() {
+    return (
+        <nav>
+            <Link href="/">Home</Link>
+            <Link href="/videos">Videos</Link>
+            <Link href="/contact">Contact</Link>
+        </nav>
+    );
+}
