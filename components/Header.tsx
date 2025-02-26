0394b0f892f1f16eb776b66ffc2d0638176003c6
Msnewbee <bilariko2@gmail.com>
2025-02-26 12:52:54 +0700

    Delete components directory

--- components/Header.tsx
+++
@@ -1,8 +0,0 @@
-// components/Header.tsx
-export default function Header({ title = "Selamat Datang di iClik Bos!" }) {
-    return (
-        <header>
-            <h1>{title}</h1>
-        </header>
-    );
-}
